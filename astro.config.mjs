36c901f4332b3970bb70d4072e9e466697813f76
Luke Walding <luke.w@me.com>
2022-12-27 10:02:28 +1100

    Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [partytown(), tailwind(), sitemap(), alpinejs()]
+});
